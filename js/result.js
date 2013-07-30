2dc33dcca3afa98614530ef71e9a9cca1ba703ec
hrut@cambium.co.il <zxczxc123>
2013-07-30 18:01:31 +0300

    app functionality

---
+++ js/result.js
@@ -0,0 +1,41 @@
+function setResultPage(data){
+    console.log("setResultPage data.status: " + data.status);
+
+    //set the dictionary text
+    $("#results-dic").text(resultsDic);
+
+      var isSingleVote ;
+      var firstFields;
+      var secondFields;
+     if(data.votes.length == 2){
+         isSingleVote = false;
+     }
+     else  if(data.votes.length == 1){
+         isSingleVote = true;
+     }
+
+     if(isSingleVote){
+         firstFields = getFielsdByVote(data.votes[1]);
+         $("#results-img-single").css("background-image","url('"+firstFields[2]+"')")
+        $("#results-comp-name-single").text(firstFields[0]);
+        $("#results-song-name-single").text(firstFields[1]);
+
+          //navigate
+         Navi.goto("resultsSingle");
+     }
+     else if(!isSingleVote){
+         firstFields = getFielsdByVote(data.votes[0]);
+         secondFields = getFielsdByVote(data.votes[1]);
+            //firat comp
+        $("#results-img-first").css("background-image","url('"+firstFields[2]+"')")
+        $("#results-comp-name-first").text(firstFields[0]);
+        $("#results-song-name-first").text(firstFields[1]);
+            //second comp
+        $("#results-img-second").css("background-image","url('"+secondFields[2]+"')")
+        $("#results-comp-name-second").text(secondFields[0]);
+        $("#results-song-name-second").text(secondFields[1]);
+
+        //navigate
+         Navi.goto("resultsBattle");
+     }
+}
---
+++ js/script.js
@@ -0,0 +1,73 @@
+$(document).ready(function() {
+    initAppSize();
+
+    //init the dictionary values
+    initDictionaryValues();
+});
+
+
+function pageChange(data){
+    var status = data.status;
+
+    switch(status){
+        case 11:
+            setStaticPage(data);
+            break;
+        case 21:
+            setOpenRegisterPage(data);
+            break;
+        case 22:
+            setRegisterGoingClose(data);
+            break;
+        case 23:
+            setVotePage(data);
+            break;
+        case 25:
+            setVoteClosePage(data);
+            break;
+        case 26:
+            setResultPage(data);
+            break;
+
+    }
+}
+
+
+
+
+
+
+var isBigSize = false;
+function initAppSize(){
+    //set the isBigSize parameter by the device
+}
+
+var registerDic = "";
+var pushVoteDic = "";
+var afterVoteDic = "";
+var resultsDic = "";
+var endShowDic = "";
+//init the dictionary values from admin- to blue title
+function initDictionaryValues(){
+    registerDic = "אתם פה? מוכנים להצביע?";
+    pushVoteDic = "הצביעו עכשיו, נשאר או הולך?";
+    afterVoteDic = "תודה על הצבעתך.";
+    resultsDic = "ויש לנו תוצאות...";
+    endShowDic = "התוכנית הסתיימה, נתראה בשלישי ב21:00";
+
+}
+
+function getFielsdByVote(voteData){
+     var firstName =voteData.name;
+     var firstSong =voteData.songName;
+     var firstUrl="";
+    //set the img by size
+    if(isBigSize){
+        firstUrl =voteData.imageUrlB;
+    }
+      else{
+        firstUrl =voteData.imageUrlA;
+    }
+    var results = new Array(firstName,firstSong,firstUrl);
+    return results;
+}
