bb4bd23eeac3f253dd33af12be37d3428f35706a
hrut@cambium.co.il <zxczxc123>
2013-08-04 11:53:56 +0300

    s

--- js/serverRequest.js
+++ js/serverRequest.js
@@ -1,12 +1,13 @@
 
 ///************long polling- get status - change the pages******************/
-////var domain = "http://192.168.2.108:8080/nextStarQA/json?";
+//var domain = "http://192.168.2.108:8080/nextStarQA/json?";
 //var domain = "http://192.168.2.108:8080/nextStarQA/userjson?type=getStatus";
-////var domain = 'http://makosrv1.egoline.co.il/nextStar/userjson?type=getStatus';
-//pollController=new PollController();
-//pollController._setUrl(domain);
-//pollController._setCallBackFunction(function(data) {
-//    console.log(data);
-//    pageChange(data);
-//});
-//pollController._init();
+//var domain = 'http://makosrv1.egoline.co.il/nextStar/userjson?';
+var domain = "http://makosrv1.egoline.co.il/nextStarTestA/userjson?";
+pollController=new PollController();
+pollController._setUrl("http://makosrv1.egoline.co.il/nextStarTestA/userjson?type=getStatus");
+pollController._setCallBackFunction(function(data) {
+    console.log(data);
+    pageChange(data);
+});
+pollController._init();
