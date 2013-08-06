3a3352e0824667276ea85a6de55546f822069ef1
reut-cambium <treut@cambium.co.il>
2013-08-06 11:16:31 +0300

    download song btn

--- js/gallery.js
+++ js/gallery.js
@@ -1,8 +1,8 @@
 function gallerySize(){
     var resultGallery=$("#result-gallery").width();
-    var singleVoteWidth = resultGallery * 45.6 / 100;
+    var singleVoteWidth = resultGallery * 31 / 100;
     var numSingleVotes = $(".gallery-vote-single").width(singleVoteWidth).size();
-    var battleVoteWidth = $("#result-gallery").width() * 75 / 100;
+    var battleVoteWidth = $("#result-gallery").width() * 50 / 100;
     var numBattleVotes = $(".gallery-vote-battle").width(battleVoteWidth).size();
     var margin = 4;
     var resultGalleryList = numSingleVotes * (singleVoteWidth + margin) + numBattleVotes * (battleVoteWidth + margin);
