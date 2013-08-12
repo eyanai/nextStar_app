f029866850e6b5c6e187bdaa409e656ed39f2665
shaigiv <shai.givati@gmail.com>
2013-08-12 17:35:39 +0300

    ג

--- js/script.js
+++ js/script.js
@@ -162,3 +162,11 @@ function initMoveEvents(){
         $(document).css("top","0px")
     };
 }
+
+function toggleTopMenu(headerText) {
+
+    $(".topMenu").slideUp(1000);
+    $(".topMenu h1").text(headerText);
+    $(".topMenu").slideDown(1000);
+
+}
