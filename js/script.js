e21727df1df50dedc9f834863281b7678c39f20b
shaigiv <shai.givati@gmail.com>
2013-08-12 16:39:49 +0300

    ג

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     //init the dictionary values
     initDictionaryValues();
    // attachEventsFacebook();
-    attachEventsRegister();
+    //attachEventsRegister();
     attachEventsVote();
     longPolling();
 
