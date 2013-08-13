33db4174c28239eed35458d64ba54dd359fc96f4
hrut@cambium.co.il <zxczxc123>
2013-08-13 14:38:06 +0300

    s

--- js/navigation.js
+++ js/navigation.js
@@ -2,13 +2,13 @@
 
 var Navi = {
 
-    hidelogin: function () {
+    hidelogin: function() {
         $('header').removeClass('hide');
         $('#login,section').hide();
     },
 
-    goto: function (el) {
-        switch (el) {
+    goto: function(el) {
+        switch(el) {
             case 'login1':
                 //
                 break;
@@ -22,6 +22,7 @@ var Navi = {
                 $('#message .messageImage').hide();
                 $('#message .messageNoImage').show();
                 $('#message .messageNoImage .continue').show();
+                $(".live").hide();
                 break;
             case 'messageWaitingWithImage':
                 Navi.hidelogin();
@@ -30,6 +31,7 @@ var Navi = {
                 $('#message .messageImage').show();
                 $('#message .messageImage .continue').show();
                 $('#message .messageNoImage').hide();
+                $(".live").hide();
                 break;
             case 'messageNoImage':
                 Navi.hidelogin();
@@ -38,6 +40,7 @@ var Navi = {
                 $('#message').show();
                 $('#message .messageImage').hide();
                 $('#message .messageNoImage').show();
+                $(".live").hide();
                 break;
             case 'messageWithImage':
                 Navi.hidelogin();
@@ -46,29 +49,32 @@ var Navi = {
                 $('#message').show();
                 $('#message .messageImage').show();
                 $('#message .messageNoImage').hide();
+                $(".live").hide();
                 break;
-            //       
+            //        
             case 'registerSingle':
                 Navi.hidelogin();
                 $('#register').show();
                 $(".topMenu").show();
-                if ($(".slidein ").hasClass('grey')) {
+                if($(".slidein ").hasClass('grey')) {
                     $(".slidein ").removeClass('grey');
                 };
                 $("#registerSingle").show();
                 $("#registerBattle").hide();
                 $(".register-red-flash").hide();
+                $(".live").hide();
                 break;
             case 'registerBattle':
                 Navi.hidelogin();
                 $('#register').show();
                 $(".topMenu").show();
-                if ($(".slidein ").hasClass('grey')) {
+                if($(".slidein ").hasClass('grey')) {
                     $(".slidein ").removeClass('grey');
                 }
                 $("#registerSingle").hide();
                 $("#registerBattle").show();
                 $(".register-red-flash").hide();
+                $(".live").hide();
                 break;
             case 'notRegister':
                 $(".slidein ").addClass('grey');
@@ -79,6 +85,7 @@ var Navi = {
                 $(".slidein").hide();
                 $(".topMenu").show();
                 $("#register .reMesseg .continue").show();
+                $(".live").hide();
                 break;
             case 'voteSingle':
                 Navi.hidelogin();
@@ -86,6 +93,7 @@ var Navi = {
                 $(".topMenu").show();
                 $("#vote .single").show();
                 $("#vote .battle").hide();
+                $(".live").show();
                 break;
             case 'voteBattle':
                 Navi.hidelogin();
@@ -93,6 +101,7 @@ var Navi = {
                 $(".topMenu").show();
                 $("#vote .single").hide();
                 $("#vote .battle").show();
+                $(".live").show();
                 break;
             case 'voteCloseSingle':
                 Navi.hidelogin();
@@ -101,6 +110,7 @@ var Navi = {
                 $("#voteClose .single").show();
                 $("#voteClose .battle").hide();
                 $("#vote-close-wait-text-single").show();
+                $(".live").hide();
                 break;
             case 'voteCloseBattle':
                 Navi.hidelogin();
@@ -109,6 +119,7 @@ var Navi = {
                 $("#voteClose .single").hide();
                 $("#voteClose .battle").show();
                 $("#vote-close-wait-text-battle").show();
+                $(".live").hide();
                 break;
             case 'resultsSingle':
                 Navi.hidelogin();
@@ -118,6 +129,7 @@ var Navi = {
                 $("#results .single").show();
                 $("#results .battle").hide();
                 $("#results .single .reMesseg .continue").show();
+                $(".live").hide();
                 break;
             case 'resultsBattle':
                 Navi.hidelogin();
@@ -126,12 +138,14 @@ var Navi = {
                 $("#results .single").hide();
                 $("#results .battle").show();
                 $("#results .battle .reMesseg .continue").show();
+                $(".live").hide();
                 break;
             case 'gallery':
                 Navi.hidelogin();
                 $(".topMenu").show();
                 $('#gallery').show();
                 gallerySize();
+                $(".live").hide();
                 break;
             default:
                 Navi.hidelogin();
