af82ff65816f51f45c95290bf46e63766c921d07
yanai <edriyanai@gmail.com>
2013-08-15 11:17:11 +0300

    battel animatiom

--- js/voteanimatiom.js
+++ js/voteanimatiom.js
@@ -24,7 +24,9 @@ $('.slideRight').draggable({
     axis: "x",
    // containment:[conleftstart,0,0,0],
     drag: function (event, ui) {
-		singelR=$(document).width()-$('.slideRight').position().left+40;
+		sR=$(document).width();
+		sR2=$('.slideRight').width();
+		singelR=sR-sR2;
     	if($('.slideRight').position().left<singelR){
 			$('.redArrow').addClass('rotupr');
 			$('.slideRight').addClass('songBad');
@@ -41,50 +43,67 @@ $('.slideRight').draggable({
 $('.slideTopbattle.con1').draggable({ //R top
 	stack: ".drag",
     axis: "x",
-   // containment:"parent" ,
+   	containment:"#rslidecon" ,
     drag: function (event, ui) {
-	
-    	//if($('.slideRight').position().left<singelR){
-//			$('.redArrow').addClass('rotupr');
-//			$('.slideRight').addClass('songBad');
-//			$('.slideLeft').addClass('hideL');
-//			setTimeout(function(){$('.redArrow.rotupr').hide();},400);
-//			$('.hate').addClass('hateShow');
-//			return false;
-//		};
+		sR=$(document).width();
+		if($('.slideTopbattle.con1').position().left<10){
+			$('.redArrow.cont1').addClass('rotuprcon1');
+			$('.slideTopbattle.con1').addClass('battel1vot');
+			$('.slideDownbattle.con1').addClass('hideRcon');
+			$('.slideTopbattle.con2,.slideDownbattle.con2').addClass('hideLcon');
+			setTimeout(function(){$('.redArrow.cont1.rotuprcon1').hide();},800);
+			$('.hate1').addClass('showIconCon1');			
+			return false;
+		};
+	},
+	stop:function (event, ui) {
+		if($('.slideTopbattle.con1').position().left>10){
+			$('.slideTopbattle.con1').animate({left:'30%'},800,function(){});	
+		}
 	}
 });
 
 $('.slideDownbattle.con1').draggable({ ///R bottom
 	stack: ".drag",
     axis: "x",
-   // containment:"parent" ,
+    containment:"#rslidecon" ,
     drag: function (event, ui) {
-	
-    	//if($('.slideRight').position().left<singelR){
-//			$('.redArrow').addClass('rotupr');
-//			$('.slideRight').addClass('songBad');
-//			$('.slideLeft').addClass('hideL');
-//			setTimeout(function(){$('.redArrow.rotupr').hide();},400);
-//			$('.hate').addClass('hateShow');
-//			return false;
-//		};
+		if($('.slideDownbattle.con1').position().left<10){
+			$('.blueArrow.cont1').addClass('rotuprcon1');
+			$('.slideDownbattle.con1').addClass('battel1vot');
+			$('.slideTopbattle.con1').addClass('hideRcon');
+			$('.slideTopbattle.con2,.slideDownbattle.con2').addClass('hideLcon');
+			setTimeout(function(){$('.blueArrow.cont1.rotuprcon1').hide();},800);
+			$('.love1').addClass('showIconCon1');
+			return false;
+		};
+	},
+	stop:function (event, ui) {
+		if($('.slideDownbattle.con1').position().left>10){
+			$('.slideDownbattle.con1').animate({left:'30%'},800,function(){});	
+		}
 	}
 });
 
 $('.slideTopbattle.con2').draggable({ //left top
 	stack: ".drag",
     axis: "x",
-   // containment:"parent" ,
+    containment:"#lslidecon" ,
     drag: function (event, ui) {
-    	if($('.slideTopbattle.con2').position().left>-5){
+    	if($('.slideTopbattle.con2').position().left>300){
 			$('.redArrow.cont2').addClass('rotuplcon2');
-		//	$('.slideLeft').addClass('songGood');
-		//	$('.slideRight').addClass('hideR');
-		//	setTimeout(function(){$('.blueArrow.rotupl').hide();},800);
-		//	$('.love').addClass('loveShow');
+			$('.slideTopbattle.con2').addClass('battel2vot');
+			$('.slideDownbattle.con2').addClass('hideLcon');
+			$('.slideTopbattle.con1,.slideDownbattle.con1').addClass('hideRcon');
+			setTimeout(function(){$('.redArrow.cont2.rotuplcon2').hide();},800);
+			$('.hate2').addClass('showIconCon2');
 			return false;
 		};
+	},
+	stop:function (event, ui) {
+		if($('.slideTopbattle.con2').position().left<300){
+			$('.slideTopbattle.con2').animate({left:'20%'},800,function(){});	
+		}
 	}
 });
 
@@ -92,16 +111,24 @@ $('.slideTopbattle.con2').draggable({ //left top
 $('.slideDownbattle.con2').draggable({ ///left bottom
 	stack: ".drag",
     axis: "x",
-   // containment:"parent" ,
+    containment:"#lslidecon" ,
     drag: function (event, ui) {
 	
-    	if($('.slideDownbattle.con2').position().left>-5){
+    	if($('.slideDownbattle.con2').position().left>300){
 			$('.blueArrow.cont2').addClass('rotuplcon2');
-		//	$('.slideLeft').addClass('songGood');
-		//	$('.slideRight').addClass('hideR');
-		//	setTimeout(function(){$('.blueArrow.rotupl').hide();},800);
-		//	$('.love').addClass('loveShow');
+			$('.slideDownbattle.con2').addClass('battel2vot');
+			$('.slideTopbattle.con2').addClass('hideLcon');
+			$('.slideTopbattle.con1,.slideDownbattle.con1').addClass('hideRcon');
+			setTimeout(function(){$('.blueArrow.cont2.rotuplcon2').hide();},800);
+			$('.love2').addClass('showIconCon2');
 			return false;
-		};
+		}
+	}, 
+	stop:function (event, ui) {
+		if($('.slideDownbattle.con2').position().left<300){
+			$('.slideDownbattle.con2').animate({left:'20%'},800,function(){});	
+		}
 	}
 });
+
+$('.slideTopbattle.con1').width();
