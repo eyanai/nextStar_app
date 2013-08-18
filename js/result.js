336aa515880b0ee5e6e3bae1e700cea77884131d
hrut@cambium.co.il <zxczxc123>
2013-08-18 15:54:04 +0300

    navigation + percent

--- js/result.js
+++ js/result.js
@@ -55,6 +55,9 @@ function setResultPage(data) {
             
             $("#results .battle .resultScalaR .result").removeClass("resultBlue");
             $("#results .battle .resultScalaR .scala").removeClass("scalaBlue");
+
+            //set the percent number color
+            $("#results-perc-first").addClass("red");
         }
         //else - remove the red class- and the blue class will shown
         else{
@@ -63,6 +66,9 @@ function setResultPage(data) {
 
             $("#results .battle .resultScalaR .result").removeClass("resultRed");
             $("#results .battle .resultScalaR .scala").removeClass("scalaRed");
+            
+            //set the percent number color
+            $("#results-perc-first").removeClass("red");
         }
              //right checked
         if(data.votes[1].finalPercent >= data.votes[1].threshold){
@@ -71,6 +77,9 @@ function setResultPage(data) {
 
             $("#results .battle .resultScalaL .result").removeClass("resultBlue");
             $("#results .battle .resultScalaL .scala").removeClass("scalaBlue");
+
+            //set the percent number color
+            $("#results-perc-second").addClass("red");
         }
        else{
            $("#results .battle .resultScalaL .result").addClass("resultBlue");
@@ -78,6 +87,9 @@ function setResultPage(data) {
 
            $("#results .battle .resultScalaL .result").removeClass("resultRed");
            $("#results .battle .resultScalaL .scala").removeClass("scalaRed");
+
+           //set the percent number color
+            $("#results-perc-second").removeClass("red");
         }
        
         $('.scala').animate({ bottom: '0%' }, 1000, function() {
