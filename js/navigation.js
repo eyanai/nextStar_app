7d958137ccf133471470d8799335dee58888aa4f
Saronet <sarahillel436@gmail.com>
2013-08-22 09:44:16 +0300

    לוגין אנדרואיד ואייפון

--- js/navigation.js
+++ js/navigation.js
@@ -202,12 +202,12 @@ var orientationEvent = supportsOrientationChange ? "orientationchange" : "resize
 
 window.addEventListener(orientationEvent, 
    function() { 
-		alert ('orientation changed'); 
+		// alert ('orientation changed'); 
 		setTimeout(function(){
-			 alert ('general');
-			 alert (window.orientation);
+			 // alert ('general');
+			 // alert (window.orientation);
 			 window.isPortrait=(window.innerHeight/window.innerWidth)>1;
-			 alert (window.isPortrait);
+			 // alert (window.isPortrait);
 			 switch (window.isPortrait) {
 				 case true:
 					 $("#horizonal-screen").hide();
