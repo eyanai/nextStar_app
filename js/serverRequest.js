56cc0cd6d160fec917eac5c655333e889c9a762e
shaigiv <shai.givati@gmail.com>
2013-09-03 15:47:56 +0300

    שינוים

--- js/serverRequest.js
+++ js/serverRequest.js
@@ -11,7 +11,7 @@ var serverDomain = "/nextStarUser/userjson?";
 //F -yerutech
 //cambium -D
 //var domain = "http://thenextstar.mako.co.il/application";
-//var domain = "http://thenextstar.mako.co.il/applicationMakoQA";
+var domain = "http://thenextstar.mako.co.il/applicationMakoQA";
 //var domain = "http://thenextstar.mako.co.il:9090/apptest";
 //var domain = "http://thenextstar.mako.co.il:9090/applicationYerutechQA";
 //var domain = "http://thenextstar.mako.co.il:9090/reut_test";//////////////check fb feed
@@ -22,7 +22,7 @@ function longPolling() {
     //alert(0);
     pollController = new PollController();
     //pollController._setUrl("http://thenextstar.mako.co.il/page/pageF.json");
-    pollController._setUrl("/page/pageF.json");
+    pollController._setUrl("/page/pageE.json");
     pollController._setCallBackFunction(function (data) {
         console.log(data);
         pageChange(data);
