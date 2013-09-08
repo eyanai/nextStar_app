0b5dbcc559464a033e472c37602fdbde8cf16589
Saronet <sarahillel436@gmail.com>
2013-09-08 15:21:19 +0300

    a

--- js/serverRequest.js
+++ js/serverRequest.js
@@ -4,14 +4,14 @@
 //var serverDomain = '/nextStar/userjson?';
 /***********PRODUCTION***********/
 
-var serverDomain = "http://thenextstar.mako.co.il/nextStarUser/userjson?";
-//var serverDomain = "/nextStarUser/userjson?";
+//var serverDomain = "http://thenextstar.mako.co.il/nextStarUser/userjson?";
+var serverDomain = "/nextStarUser/userjson?";
 
 //E -mako
 //F -yerutech
 //cambium -D
+var domain = "http://thenextstar.mako.co.il/applicationMakoQA";
 //var domain = "http://thenextstar.mako.co.il/application";
-var domain = "http://thenextstar.mako.co.il/application";
 //var domain = "http://thenextstar.mako.co.il:9090/apptest";
 //var domain = "http://thenextstar.mako.co.il:9090/applicationYerutechQA";
 //var domain = "http://thenextstar.mako.co.il:9090/reut_test";//////////////check fb feed
@@ -22,7 +22,7 @@ function longPolling() {
     //alert(0);
     pollController = new PollController();
     //pollController._setUrl("http://thenextstar.mako.co.il/page/pageF.json");
-    pollController._setUrl("/page/pageD.json");
+    pollController._setUrl("/page/pageF.json");
     pollController._setCallBackFunction(function (data) {
         console.log(data);
         pageChange(data);
