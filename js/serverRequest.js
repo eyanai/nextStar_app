44bca59d57b7674036d4f81651ce6e1293364339
reut-cambium <treut@cambium.co.il>
2013-09-15 14:18:26 +0300

    try fb

--- js/serverRequest.js
+++ js/serverRequest.js
@@ -4,25 +4,25 @@
 //var serverDomain = '/nextStar/userjson?';
 /***********PRODUCTION***********/
 
-var serverDomain = "http://thenextstar.mako.co.il/nextStarUser/userjson?";
-//var serverDomain = "/nextStarUser/userjson?";
+//var serverDomain = "http://thenextstar.mako.co.il/nextStarUser/userjson?";
+var serverDomain = "/nextStarUser/userjson?";
 
 //E -mako
 //F -yerutech
 //cambium -D
-var domain = "http://thenextstar.mako.co.il/applicationMakoQA";
+var domain = "http://thenextstarb.mako.co.il/applicationMakoQA";
 //var domain = "http://thenextstar.mako.co.il/application";
 //var domain = "http://thenextstar.mako.co.il:9090/apptest";
 //var domain = "http://thenextstar.mako.co.il:9090/applicationYerutechQA";
 //var domain = "http://thenextstar.mako.co.il:9090/reut_test";//////////////check fb feed
-//var fbPostPicDomain=;
+var fbPostPicDomain="http://thenextstarb.mako.co.il/applicationMakoQA" ;
 
 
 function longPolling() {
     //alert(0);
     pollController = new PollController();
-       pollController._setUrl("http://thenextstar.mako.co.il/page/pageF.json");
- //pollController._setUrl("/page/pageF.json");
+       // pollController._setUrl("http://thenextstar.mako.co.il/page/pageF.json");
+pollController._setUrl("/page/pageF.json");
     pollController._setCallBackFunction(function (data) {
         console.log(data);
         pageChange(data);
